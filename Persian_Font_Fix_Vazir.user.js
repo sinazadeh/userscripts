36a997d770bf809297d54be6b1eaa9c668aa3c66
Sina Zadeh <s.hossinzade@gmail.com>
2025-07-06 18:31:31 -0500

    Fix downloadURL and updateURL

--- Persian_Font_Fix_Vazir.user.js
+++ Persian_Font_Fix_Vazir.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Persian Font Fix (Vazir)
 // @namespace    https://github.com/sinazadeh/userscripts
-// @version      2.0.5
+// @version      2.0.6
 // @description  Apply Vazir font to Persian/RTL content across selected websites
 // @author       TheSina
 // @match       *://*.telegram.org/*
