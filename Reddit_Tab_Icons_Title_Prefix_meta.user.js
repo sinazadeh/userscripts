10bc089239608450dc26b0261aa90298da3c6c27
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-07 20:46:28 +0000

    chore: update userscript meta

---
+++ Reddit_Tab_Icons_Title_Prefix_meta.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Reddit Tab Icons & Title Prefix
+// @namespace https://github.com/sinazadeh/userscripts
+// @version      1.3.2
+// @description  Efficiently fetch & cache subreddit favicons and always prefix the tab title with r/Name on reddit.
+// @author       TheSina
+// @match        https://*.reddit.com/*
+// @exclude      https://*.reddit.com/account/*
+// @grant        none
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Reddit_Tab_Icons_Title_Prefix.js
+// @updateURL    https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Reddit_Tab_Icons_Title_Prefix_meta.user.js
+// ==/UserScript==
