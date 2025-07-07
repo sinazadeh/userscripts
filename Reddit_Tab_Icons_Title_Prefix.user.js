731f395961bf849d1637043aa8641204e828cd48
Sina Zadeh <s.hossinzade@gmail.com>
2025-07-07 15:48:27 -0500

    chore: bump version to 1.3.4 and update download URL

--- Reddit_Tab_Icons_Title_Prefix.user.js
+++ Reddit_Tab_Icons_Title_Prefix.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Reddit Tab Icons & Title Prefix
 // @namespace https://github.com/sinazadeh/userscripts
-// @version      1.3.3
+// @version      1.3.4
 // @description  Efficiently fetch & cache subreddit favicons and always prefix the tab title with r/Name on reddit.
 // @author       TheSina
 // @match        https://*.reddit.com/*
 // @exclude      https://*.reddit.com/account/*
 // @grant        none
 // @license      MIT
-// @downloadURL  https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Reddit_Tab_Icons_Title_Prefix.js
+// @downloadURL  https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Reddit_Tab_Icons_Title_Prefix.user.js
 // @updateURL    https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Reddit_Tab_Icons_Title_Prefix_meta.user.js
 // ==/UserScript==
 
