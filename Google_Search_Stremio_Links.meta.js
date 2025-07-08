8fc078faa6212bb10fdb251d2abd507cd8525ed2
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-08 02:50:13 +0000

    chore: update userscript meta

--- Google_Search_Stremio_Links.meta.js
+++ Google_Search_Stremio_Links.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Google Search: Stremio Links
 // @namespace    https://github.com/sinazadeh/userscripts
-// @version      1.2.8
+// @version      1.2.9
 // @description  Adds convenient "Watch on Stremio" buttons (App & Web) next to IMDb links in Google search results.
 // @author       TheSina
 // @match        *://www.google.*/*
 // @exclude      *://*.google.*/recaptcha/*
+// @grant        GM_addStyle
 // @run-at       document-end
 // @license      MIT
 // @downloadURL  https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Google_Search_Stremio_Links.user.js
