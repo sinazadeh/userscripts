937f1d6059dc48ed4ec54baf8ef14771f2e74c06
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-08 17:58:16 +0000

    chore: update userscript meta

--- Amazon_Filter_Sold_by_Amazoncom.meta.js
+++ Amazon_Filter_Sold_by_Amazoncom.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Amazon Filter: Sold by Amazon.com
 // @namespace    https://github.com/sinazadeh/userscripts
-// @version      1.0.0
+// @version      1.1.0
 // @description  Adds a persistent dropdown to Amazon search and category pages to filter products by seller (e.g., 'Sold by Amazon.com', 'All Amazon').
 // @author       TheSina
 // @match        *://www.amazon.com/s*
 // @match        *://www.amazon.com/*/b/*
+// @match        *://www.amazon.com/b/*
 // @grant        none
 // @run-at       document-end
 // @license      MIT
