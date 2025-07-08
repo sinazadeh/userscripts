9e32886164664f329c46e050e732d26e9e7306c9
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-08 17:49:17 +0000

    chore: update userscript meta

--- Amazon_Filter_Sold_by_Amazoncom.meta.js
+++ Amazon_Filter_Sold_by_Amazoncom.meta.js
@@ -9,4 +9,6 @@
 // @grant        none
 // @run-at       document-end
 // @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Amazon_Filter_Sold_by_Amazoncom.user.js
+// @updateURL    https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Amazon_Filter_Sold_by_Amazoncom.meta.js
 // ==/UserScript==
