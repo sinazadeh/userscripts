7be8ed9c8800f32a98f7552dc3e81b3d7e745311
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-13 17:19:24 +0000

    chore: update userscript meta

---
+++ Xbox_PriceLens.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Xbox PriceLens
+// @namespace    https://github.com/sinazadeh/userscripts
+// @version      1
+// @description  Get a clear view of global Xbox pricing. PriceLens adds a powerful, customizable dashboard to game pages, showing you what a game costs in different countries—all in your home currency. Pin your favorite stores and let PriceLens help you focus on the best deals.
+// @author       TheSina
+// @match        *://www.xbox.com/*/games/store/*
+// @connect      cdn.jsdelivr.net
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Xbox_PriceLens.user.js
+// @updateURL    https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Xbox_PriceLens.meta.js
+// ==/UserScript==
