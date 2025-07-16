de88a7916be25ea7dca2dec1e4d12a695c248d7b
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-16 18:23:29 +0000

    chore: update userscript meta

--- GitHub_Tab_Avatar.meta.js
+++ GitHub_Tab_Avatar.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Tab Avatar
 // @namespace    https://github.com/sinazadeh/userscripts
-// @version      1.0.1
+// @version      1.0.2
 // @description  Use each GitHub repository’s avatar as the browser tab icon.
 // @author       TheSina
 // @match        *://github.com/*/*
