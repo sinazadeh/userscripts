efc164d09083fb58ac3c6f2966131784681f2d29
sinazadeh <sinazadeh@users.noreply.github.com>
2025-07-18 19:13:15 +0000

    chore: update userscript meta

---
+++ Noto_Sans_for_Google.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Noto Sans for Google
+// @namespace    https://github.com/sinazadeh/userscripts
+// @version      1.0.0
+// @description  Replaces Arial and Roboto with the locally installed Noto Sans font on Google websites.
+// @author       TheSina
+// @match        *://*.google.*/*
+// @exclude      *://*.google.*/recaptcha/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// @license      MIT
+// @downloadURL  https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Noto_Sans_for_Google.user.js
+// @updateURL    https://raw.githubusercontent.com/sinazadeh/userscripts/refs/heads/main/Noto_Sans_for_Google.meta.js
+// ==/UserScript==
